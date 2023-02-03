769531fea87af2971bf892d0c3151ee22422fad1
Jacob Michaelis <jacobtmichaelis@gmail.com>
2023-02-03 15:24:33 -0700

    Added scripts from my poc drag and drop project

---
+++ Assets/_Project/_Scripts/Draggable.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable :
+    MonoBehaviour, IPointerDownHandler,
+    IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+  [SerializeField] private Canvas canvas;
+  [SerializeField] private GameObject draggingZone;
+  private CanvasGroup cardCanvasGroup;
+  [SerializeField] private GameObject towerState;
+  [SerializeField] private GameObject cardState;
+
+  public void Awake() {
+    cardCanvasGroup = GetComponent<CanvasGroup>();
+    towerState.SetActive(false);
+  }
+
+  public void OnBeginDrag(PointerEventData eventData) {
+    Debug.Log("OnBeginDrag");
+    cardCanvasGroup.blocksRaycasts = false;
+    cardCanvasGroup.alpha = 0.6f;
+    this.transform.SetParent(draggingZone.transform);
+  }
+
+  public void OnDrag(PointerEventData eventData) {
+    this.transform.position = eventData.position;
+  }
+
+  public void OnEndDrag(PointerEventData eventData) {
+    Debug.Log("OnEndDrag");
+    cardCanvasGroup.blocksRaycasts = true;
+    cardCanvasGroup.alpha = 1f;
+  }
+
+  public void OnPointerDown(PointerEventData eventData) {
+    Debug.Log("OnPointerDown");
+  }
+
+  public void PlaceTroopState() {
+    cardState.SetActive(false);
+    towerState.SetActive(true);
+  }
+
+  public void MoveCardState() {
+    cardState.SetActive(true);
+    towerState.SetActive(false);
+  }
+}
